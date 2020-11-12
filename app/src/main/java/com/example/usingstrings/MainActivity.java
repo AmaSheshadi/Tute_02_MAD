8ad0db9cec7706588ea3d468ac1f41d0f7907403
amasheshadee <53006150+amasheshadee@users.noreply.github.com>
2020-11-13 00:42:01 +0530

    use of logcat(Activity lifecycle)

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Lifecycle", "OnCreate() invoked(Oncreate calling) ");
+
+        TextView textView2 = findViewById(R.id.TvMsg2);
+        textView2.setText(R.string.Msg1);
+
+        TextView textView = findViewById(R.id.TvMsg1);
+        textView2.setText(R.string.Msg2);
+
+
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.i("Lifecycle", "OnStart() invoked ");
+    }
+
+    @Override
+    public void onRestart()
+    {
+        super.onRestart();
+        Log.i("Lifecycle", " OnRestart() invoked");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.i("Lifecycle", "  OnPause() invoked");
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.i("Lifecycle", "  onResume() invoked");
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.i("Lifecycle", "  onResume() invoked");
     }
 }
